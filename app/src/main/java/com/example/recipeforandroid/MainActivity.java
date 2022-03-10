7e7cd7619b9d1c9c579b15190ea495d3c9d6f0cd
Ramondlr <42787384+Ramondlr@users.noreply.github.com>
2022-03-10 13:31:27 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/recipeforandroid/MainActivity.java
+++ app/src/main/java/com/example/recipeforandroid/MainActivity.java
@@ -3,17 +3,12 @@ package com.example.recipeforandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
     }
-
 }
