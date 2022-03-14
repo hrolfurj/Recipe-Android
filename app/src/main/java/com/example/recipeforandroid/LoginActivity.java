6cf11d81e1d777c8d24fffa62032d95a2d9a469f
hrolfurj <hrolfur@hi.is>
2022-03-14 13:58:10 +0000

    rename klasa

--- app/src/main/java/com/example/recipeforandroid/LoginActivity.java
+++ app/src/main/java/com/example/recipeforandroid/LoginActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
                     //correct
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    Intent intent = new Intent(LoginActivity.this, RecipeListActivity.class);
                     startActivity(intent);
                 } else
                     //incorrect
