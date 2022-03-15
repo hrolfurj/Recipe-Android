db770898e1abe19969318f14b86373f22c413b75
hrolfurj <hrolfur@hi.is>
2022-03-15 15:35:32 +0000

    login status vistaður, fer beint á RecipeList ef logged in

--- app/src/main/java/com/example/recipeforandroid/LoginActivity.java
+++ app/src/main/java/com/example/recipeforandroid/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.recipeforandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +27,19 @@ public class LoginActivity extends AppCompatActivity {
 
         MaterialButton login_button = (MaterialButton) findViewById(R.id.login_button);
 
+        sp = getSharedPreferences("login", MODE_PRIVATE);
+
+        if(sp.getBoolean("logged", false)){
+            goToRecipeList();
+        }
+
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
                     //correct
-                    Intent intent = new Intent(LoginActivity.this, RecipeListActivity.class);
-                    startActivity(intent);
+                    goToRecipeList();
+                    sp.edit().putBoolean("logged", true).apply();
                 } else
                     //incorrect
                     Toast.makeText(LoginActivity.this, R.string.login_failed_toast, Toast.LENGTH_SHORT).show();
@@ -45,4 +54,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+     public  void  goToRecipeList() {
+         Intent intent = new Intent(LoginActivity.this, RecipeListActivity.class);
+         startActivity(intent);
+     }
 }
