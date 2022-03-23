6569af42700dca19e75f41e05f70be0c0466267b
hrolfurj <hrolfur@hi.is>
2022-03-23 23:53:08 +0000

    hægt að sækja mynd í NewRecipe

--- app/src/main/java/com/example/recipeforandroid/NewRecipeActivity.java
+++ app/src/main/java/com/example/recipeforandroid/NewRecipeActivity.java
@@ -1,14 +1,28 @@
 package com.example.recipeforandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.InputStream;
 
 public class NewRecipeActivity extends AppCompatActivity {
     Button save_Button;
+    Button uploadButton;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +30,27 @@ public class NewRecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_recipe);
         getSupportActionBar().hide();
         save_Button = findViewById (R. id.save_Button);
+        uploadButton = findViewById(R.id.upload_image_button);
+        image = findViewById(R.id.new_recipe_image);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean pick = true;
+                if (pick == true){
+                    if (!checkCameraPermission()){
+                        requestCameraPermission();
+                    } else PickImage();
+                } else {
+                    if (!checkStoragePermission()) {
+                        requestStoragePermission();
+
+                    } else PickImage();
+                }
+            }   
+        });
+
+
 
         save_Button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +60,47 @@ public class NewRecipeActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void PickImage() {
+        CropImage.activity().start(this);
+    }
+
+    private void requestStoragePermission() {
+        requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+    }
+
+    private void requestCameraPermission() {
+        requestPermissions(new String[] {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
+        return result2;
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED;
+        boolean result2 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
+        return result1 && result2;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                try{
+                    InputStream stream = getContentResolver().openInputStream(resultUri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(stream);
+                    image.setImageBitmap(bitmap);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
